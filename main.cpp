9d2645694db111a290362e8e699bff2027874a91
UyenThu <24110348@student.hcmute.edu.vn>
2025-08-21 22:15:57 +0700

    Bài mới: tính tổng số nguyên tố <= n

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
-    cout << "Hello GitHub!" << endl;
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    long long sum = 0;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) sum += i;
+    }
+
+    cout << "Tong cac so nguyen to <= " << n << " la: " << sum << endl;
     return 0;
 }
